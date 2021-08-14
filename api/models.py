3e28b7505ac9a945027b28afd4a0306e66c5c2e9
Sashank Mishra <sashankmishra27@gmail.com>
2021-08-14 09:29:56 +0530

    add basic model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+
+    # video and channel ID
+    video_id = models.CharField(max_length=100)
+    channel_id = models.CharField(max_length=100)
+
+    # Text Entries related to particular video
+    title = models.CharField(null=True, blank=True, max_length=500)
+    description = models.TextField(null=True, blank=True)
+    channel_title = models.CharField(null=True, blank=True, max_length=500)
+
+    # Timestamp entries for a video
+    publish_time = models.DateTimeField()
+    create_time = models.DateTimeField(auto_now_add=True)
