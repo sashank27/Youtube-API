96aaa24daaca6d995e9c506f5856387630c66aef
Sashank Mishra <sashankmishra27@gmail.com>
2021-08-14 11:57:26 +0530

    add DRF support for API

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.video_list, name='video_list'),
+    path('', views.VideoViewSet.as_view({'get': 'list'})),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import VideoSerializer
+from .models import Video
+
+
+class VideoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows video list to be viewed
+    """
+    queryset = Video.objects.all().order_by('-create_time')
+    serializer_class = VideoSerializer
